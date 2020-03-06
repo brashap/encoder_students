c818115e0a016b5333383529884bd173cd9cdd62
Brian <brashap@cnm.edu>
2020-03-06 07:41:14 -0700

    first commit

---
+++ 07_encoder_read/07_encoder_read.ino
@@ -0,0 +1,33 @@
+/*
+ * Project: Encoder_Read
+ * Description: Learn to use an encoder by displaying position on Serial.monitor
+ * Author: 
+ * Date: 
+ */
+
+/* ASSIGNMENT
+ *  In your labbook, draw your encoder with A and B  connected to pins 2 and 3.
+ *  Transfer this to fritzing
+ *  Wire your board
+ *  Write code read the encoder position and display to the serial monitor
+ */
+ 
+// Include encoder library and set-up object
+
+
+
+
+void setup() {
+
+// Turn on Serial Monitor
+
+
+}
+
+void loop() {
+
+// Read encoder position
+// Display to screen if changed
+
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Repository for student encoder assignments
